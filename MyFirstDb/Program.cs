8b8314dc11c7ce10c58c35347947193b1722e5c2
R3ap4r <R3ap4r@telenet.be>
2021-11-11 16:46:14 +0100

    Inital commit

---
+++ MyFirstDb/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+internal static class Program
+{
+
+    public static void Main()
+        {
+        Console.WriteLine("Hello World.");
+        }
+}
