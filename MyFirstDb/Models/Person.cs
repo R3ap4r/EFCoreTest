eea3f587b28f2dd7cd8c4772287b776ee0b19784
R3ap4r <R3ap4r@telenet.be>
2021-11-11 17:28:19 +0100

    Adding Models folder with 2 test models

---
+++ MyFirstDb/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+
+namespace MyFirstDb.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public ICollection<Adress> Adresses { get; set; }
+    }
+    public class Adress
+    {
+        public int? Id { get; set; }
+        public string StreetName { get; set; }
+        public int Number { get; set; }
+        public int? Bus { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
